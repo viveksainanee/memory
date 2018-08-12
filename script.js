322d0d45f562ec6f5987831459d039b722077405
Vivek Sainanee <viveksainanee+1@gmail.com>
2018-08-12 15:28:22 +0200

    refactored to use flip class

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 window.onload = function() {
   //create board
   var board = document.getElementById('board');
-  var boardsize = 18;
-  var boardImages = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9'
-  ];
+  var boardsize = 6;
+  var boardImages = ['1', '2', '1', '2', '3', '3'];
 
   function createCards(size) {
     for (var i = 0; i < size; i++) {
+      //create scene
+      var scene = document.createElement('div');
+      scene.setAttribute('class', 'scene scene--card');
+
+      //create card container
       var card = document.createElement('div');
       card.setAttribute('class', 'card');
-      //pick the picture it is
+      scene.appendChild(card);
+
+      //create front of card
+      var front = document.createElement('div');
+      front.setAttribute('class', 'card__face card__face--front');
+      front.style.backgroundImage = "url('img/0.jpg')";
+      card.appendChild(front);
+
+      //create back of card
+      var back = document.createElement('div');
+      back.setAttribute('class', 'card__face card__face--back');
+      //pick the picture randomly
       var randomImageIndex = getRandomInt(0, boardImages.length - 1);
-      card.innerText = boardImages[randomImageIndex];
+      back.innerText = boardImages[randomImageIndex];
       boardImages.splice(randomImageIndex, 1);
+      back.style.backgroundImage = "url('img/" + back.innerText + '.jpg';
+      card.appendChild(back);
 
-      card.style.backgroundImage = "url('img/0.jpg')";
-
-      board.appendChild(card);
+      board.appendChild(scene);
     }
   }
 
@@ -44,62 +41,54 @@ window.onload = function() {
   var score = document.getElementById('score');
   score.innerText = 0;
 
-  //create timerArray
-  var timerArray = [];
+  //create timers Array
+  var timersArr = [];
 
-  //when user opens a card:
+  //when user opens a card on the board
   board.addEventListener('click', function(event) {
-    if (event.target.className == 'card') {
-      //make sure only 2 can be opened at once
-      var currentFlipped = document.querySelectorAll('.flipping');
+    // if the card is incomplete (card) or flipped, then switch it to the other position
+    if (
+      event.target.parentElement.classList == 'card' ||
+      event.target.parentElement.classList == 'card is-flipped'
+    ) {
+      event.target.parentElement.classList.toggle('is-flipped');
+    }
 
-      if (currentFlipped.length > 1) {
-        for (var i = 0; i < currentFlipped.length; i++) {
-          currentFlipped[i].style.backgroundImage = "url('img/0.jpg')";
-          currentFlipped[i].setAttribute('class', 'card');
-        }
-      }
-      //mark it as 'flipping' via class, and show the secret image
-      event.target.setAttribute('class', 'flipping card');
-      event.target.style.backgroundImage =
-        "url('img/" + event.target.innerText + '.jpg';
-      //increase the score count every time a card successfully flips
-      score.innerText++;
-
-      var currentFlipped = document.querySelectorAll('.flipping');
-      if (currentFlipped.length == 2) {
-        if (currentFlipped[0].innerText == currentFlipped[1].innerText) {
-          //if two cards have the same secret value (innerText)
-          while (timerArray.length > 0) {
-            clearTimeout(timerArray[0]);
-          }
-
-          currentFlipped[0].style.backgroundImage =
-            "url('img/" + event.target.innerText + '.jpg';
-          currentFlipped[0].setAttribute('class', 'completed card');
-
-          currentFlipped[1].style.backgroundImage =
-            "url('img/" + event.target.innerText + '.jpg';
-          currentFlipped[1].setAttribute('class', 'completed card');
-        } else {
-          //reset the card after two seconds
-          var timerId = setTimeout(function() {
-            event.target.style.backgroundImage = "url('img/0.jpg')";
-            event.target.setAttribute('class', 'card');
-          }, 2000);
-          timerArray.push(timerId);
-          console.log('incorrect card');
-          console.log(timerArray);
-        }
-      } else {
-        //reset the card after two seconds
-        var timerId = setTimeout(function() {
-          event.target.style.backgroundImage = "url('img/0.jpg')";
-          event.target.setAttribute('class', 'card');
-        }, 2000);
-        console.log('first click timer started');
+    //make sure only 2 can be opened at once
+    var currentFlipped = document.querySelectorAll('.is-flipped');
+    if (currentFlipped.length > 2) {
+      //close all cards if the user attempts to open a third
+      for (var i = 0; i < currentFlipped.length; i++) {
+        currentFlipped[i].setAttribute('class', 'card');
       }
     }
+
+    //increase the score every time a user clicks a card (that is not flipped or completed)
+    if (event.target.parentElement.classList == 'card is-flipped') {
+      score.innerText++;
+    }
+
+    //if two cards have the same secret value (innerText)`, mark as complete
+    if (currentFlipped.length == 2) {
+      if (currentFlipped[0].innerText == currentFlipped[1].innerText) {
+        currentFlipped[0].setAttribute('class', 'card completed');
+        currentFlipped[1].setAttribute('class', 'card completed');
+      }
+    }
+
+    //clear previous timers
+    for (var l = 0; l < timersArr.length; l++) {
+      clearTimeout(timersArr[l]);
+    }
+
+    //after two seconds, flip back any cards that are still marked as flipped
+    var timerID = setTimeout(function() {
+      var timeoutFlips = document.querySelectorAll('.is-flipped');
+      for (var k = 0; k < timeoutFlips.length; k++) {
+        timeoutFlips[k].setAttribute('class', 'card');
+      }
+    }, 2000);
+    timersArr.push(timerID);
   });
 
   //used for board creation
